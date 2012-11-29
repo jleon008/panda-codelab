0baa6d54fa8e3fdbcd63c401d7e5e25cd01a6cd3
Zhifeng Sun <zhsun@google.com>
2012-11-29 13:55:15 -0800

    Check in gtest used by all codelabs. Skeleton for merge sort.

---
+++ codelab/sort_merge/mergesort.h
@@ -0,0 +1,12 @@
+#ifndef CODELAB_MERGESORT_H_
+#define CODELAB_MERGESORT_H_
+
+namespace codelab {
+
+template<typename Iter1, typename Iter2 = Iter1>
+void merge_sort(Iter1 begin, Iter1 end, Iter2 out) {
+}
+
+}  // codelab
+
+#endif  // CODELAB_MERGESORT_H_
---
+++ codelab/sort_merge/mergesort_test.cc
@@ -0,0 +1,10 @@
+#include "mergesort.h"
+#include "gtest/gtest.h"
+
+namespace codelab {
+
+TEST(MergeSortTest, Hello) {
+  EXPECT_TRUE(true);
+}
+
+}
